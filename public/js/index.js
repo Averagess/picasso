0bbb39c420d97a81ba45e77f58bfdf52fa0fba05
Averagess <45748163+Averagess@users.noreply.github.com>
2021-05-30 18:05:21 +0300

    Design changes

--- public/js/index.js
+++ public/js/index.js
@@ -4,19 +4,5 @@ onload = async function() {
         const time = new Date().toLocaleTimeString();
         clockElement.innerHTML = time;
     }, 1000);
-    const sleep = (milliseconds) => {
-        return new Promise(resolve => setTimeout(resolve, milliseconds))}
-    const text = document.getElementById("header");
-    const final = "Main page is under maintenance.";
-
-    if (performance.getEntriesByType("navigation")[0].type === "reload") {
-        text.innerHTML = final;
-    }
-    else {
-        for (let char in final){
-            const ms = Math.floor(Math.random() * 50);
-            await sleep(ms)
-            text.innerHTML += final[char];
-    }};
 };
     
